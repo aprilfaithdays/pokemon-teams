53b8a6180b072c3c57c1205c7f8c7cb013e17906
aprilfaithdays <aprilfaith.e@gmail.com>
2020-03-18 09:51:31 -0400

    first commit

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,12 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("DOM loaded!")
+
+    fetch(TRAINERS_URL)
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+})
